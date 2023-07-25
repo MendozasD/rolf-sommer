80831876c2dd999a59f1a81e3d56a1a2ece943a0
David Mendoza <dmendozasommer@gmail.com>
2023-07-25 15:58:49 +0200

    Animation for Kommende cards when hover finished

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,27 +1,51 @@
 module ApplicationHelper
+  # THIS HELPER TAKE SVG FILES FROM ASSETS FOLDER AND RENDERS THEM
+  # <%= embedded_svg('icon-grid') %>
+  # <%= embedded_svg('icon-grid', class: 'my-class') %>
+  def embedded_svg(filename, options = {})
+    assets = Rails.application.assets
+    asset = assets.find_asset(filename)
+
+    if asset
+      file = asset.source.force_encoding("UTF-8")
+      doc = Nokogiri::HTML::DocumentFragment.parse file
+      svg = doc.at_css "svg"
+      svg["class"] = options[:class] if options[:class].present?
+    else
+      doc = "<!-- SVG #{filename} not found -->"
+    end
+
+    raw doc
+  end
+  # --------------------------------------------------------------------------------------------------------
   # ADDING 'ACTIVE' TO ELEMENTS
   def current_class?(test_path)
     return 'active' if request.path == test_path
     ''
   end
 
+  # --------------------------------------------------------------------------------------------------------
+
   def cl_img(g)
     if g.actor
-        'https://res.cloudinary.com/dm4hnoj7v/image/upload/v1690207472/rolf_sommer/star_neu_s_kdx5uw.svg'
+      'https://res.cloudinary.com/dm4hnoj7v/image/upload/v1690207472/rolf_sommer/star_neu_s_kdx5uw.svg'
     elsif g.director
-        'https://res.cloudinary.com/dm4hnoj7v/image/upload/v1690207472/rolf_sommer/star_neu_r_igcrfo.svg'
+      'https://res.cloudinary.com/dm4hnoj7v/image/upload/v1690207472/rolf_sommer/star_neu_r_igcrfo.svg'
     else g.texter
-        'https://res.cloudinary.com/dm4hnoj7v/image/upload/v1690207472/rolf_sommer/star_neu_t_ctanq7.svg'
+      'https://res.cloudinary.com/dm4hnoj7v/image/upload/v1690207472/rolf_sommer/star_neu_t_ctanq7.svg'
     end
   end
 
-  def work_as(g)
+  # --------------------------------------------------------------------------------------------------------
+  #FILTER SVG STICKERS FOR KOMMENDE SECTION
+  def sticker_srt(g)
     if g.actor
-      'S'
+      'actor-sticker'
     elsif g.director
-      'R'
+      'director-sticker'
     else g.texter
-      'T'
+      'texter-sticker'
     end
   end
+
 end
