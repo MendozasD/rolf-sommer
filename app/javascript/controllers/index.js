c630787f2b4acd4231c462ca723ea1e56f7801da
David Mendoza <dmendozasommer@gmail.com>
2023-08-28 15:08:37 +0200

    Modify js index controllers

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,28 +2,22 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application"
+import { application } from "./application";
 
-import BorderController from "./border_controller"
-application.register("border", BorderController)
+import BorderController from "./border_controller";
+application.register("border", BorderController);
 
-import DropdownController from "./dropdown_controller"
-application.register("dropdown", DropdownController)
+import DropdownController from "./dropdown_controller";
+application.register("dropdown", DropdownController);
 
-import FilterController from "./filter_controller"
-application.register("filter", FilterController)
+import FilterController from "./filter_controller";
+application.register("filter", FilterController);
 
-import MagneticElementsController from "./magnetic_elements_controller"
-application.register("magnetic-elements", MagneticElementsController)
+import MagneticElementsController from "./magnetic_elements_controller";
+application.register("magnetic-elements", MagneticElementsController);
 
-import MailButtonController from "./mail_button_controller"
-application.register("mail-button", MailButtonController)
+import ScrollController from "./scroll_controller";
+application.register("scroll", ScrollController);
 
-import MarqueeController from "./marquee_controller"
-application.register("marquee", MarqueeController)
-
-import ScrollController from "./scroll_controller"
-application.register("scroll", ScrollController)
-
-import VanillaTiltController from "./vanilla_tilt_controller"
-application.register("vanilla-tilt", VanillaTiltController)
+import VanillaTiltController from "./vanilla_tilt_controller";
+application.register("vanilla-tilt", VanillaTiltController);
