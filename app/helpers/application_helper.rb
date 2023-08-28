c454d9083c70ca79eb96136d55a03a0060bb5d1b
David Mendoza <dmendozasommer@gmail.com>
2023-08-28 15:41:02 +0200

    SVg tag erased

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,21 +1,4 @@
 module ApplicationHelper
-  # THIS HELPER TAKE SVG FILES FROM ASSETS FOLDER AND RENDERS THEM
-  # <%= embedded_svg('icon-grid') %>
-  # <%= embedded_svg('icon-grid', class: 'my-class') %>
-  def embedded_svg(filename, options = {})
-    assets = Rails.application.assets
-    asset = assets.find_asset(filename)
-    if asset
-      file = asset.source.force_encoding("UTF-8")
-      doc = Nokogiri::HTML::DocumentFragment.parse file
-      svg = doc.at_css "svg"
-      svg["class"] = options[:class] if options[:class].present?
-    else
-      doc = "<!-- SVG #{filename} not found -->"
-    end
-
-    raw doc
-  end
   # --------------------------------------------------------------------------------------------------------
   # ADDING 'ACTIVE' TO ELEMENTS
   def current_class?(test_path)
